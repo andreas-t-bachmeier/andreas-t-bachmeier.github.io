d1dfcbef462a66c0cbd88ca3e8503f907e553207
ANdte <104008351+andreas-t-bachmeier@users.noreply.github.com>
2025-02-23 15:35:50 +0100

    Update cookiebanner.js

--- JS/cookiebanner.js
+++ JS/cookiebanner.js
@@ -51,7 +51,7 @@
     // Create text paragraph
     var para = document.createElement("p");
     para.innerHTML = "This website uses cookies to ensure you get the best experience on our website. " +
-                     "<a href='https://inspyrall.uwu.ai/#privacy' target='_blank' style='color:#FF6400; text-decoration:underline;'>Review Cookie Policy</a>";
+                     "<a href='https://inspyrall.uwu.ai/#privacy' target='_blank' style='color:#FF6400; text-decoration:underline;'>Review Privacy Policy</a>";
     banner.appendChild(para);
     
     // Create a container for buttons
